a02f398e82e87791f268787d68328d07fb39def4
Nha Tu Le <lntu.btstudy@gmail.com>
2025-02-10 13:20:57 +0100

    fasta-file-processor.sh

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -6,22 +6,25 @@ if [ "$#" -ne "1" ]; then
     exit
 fi
 
+if [ "$num_seq" -eq 0 ] || [ -z "$total_length" ] || [ "$total_length" -eq 0 ]; then
+    echo "Error: No sequences found or total length is zero."
+    exit 1
+fi
+
 sequences=$(awk '!/^>/{print}' "$1")
-num_seq=$(grep -c '^>' $1)
+num_seq=$(grep -c '>' $1)
 total_length=$(awk '/^>/ {next} {total += length($0)} END {print total}' "$1")
 length=$(awk '/^>/ {next} {print length}' "$1") 
 max_length=$(echo "$length" | sort -nr | head -n1)
 min_length=$(echo "$length" | sort -n | head -n1)
-#avg_length=$((total_length / num_seq))
-avg_length=$(awk "BEGIN {print $total_length / $num_seq}")
-gc_count= $(echo "$sequences" | grep -o '[GCgc]' | wc -l)
-#gc_content= echo "$gc_count * 100 / $total_length" | bc -l
-gc_content=$(awk "BEGIN {print ($gc_count * 100) / $total_length}")
+avg_length=$(echo "scale=2 ; $total_length / $num_seq" | bc)
+gc_count=$(echo "$sequences" | grep -o '[GC]' | wc -l)
+gc_content=$(echo "$gc_count * 100 / $total_length" | bc -l)
 
 echo "Number of sequences: $num_seq"
 echo "Total length of sequences: $total_length"
 echo "Length of the longest sequence: $max_length"
 echo "Length of the shortest sequence: $min_length"
 echo "Average sequence length: $avg_length"
-echo "GC Content (%): $gc_count"
+echo "GC Content (%): $gc_content"
 
